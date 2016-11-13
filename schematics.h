6ec248e32e02a41918f0083b082ca43d44022e19
Brenton Cozby <brenton.cozby@gmail.com>
2016-11-12 23:09:34 -0500

    Create schematics.h

---
+++ schematics.h
@@ -0,0 +1,17 @@
+typedef struct cut // for "measurement"
+{
+    char* dimensions;
+    float length;
+    float quantity;
+}cut;
+
+typedef struct lumber
+{
+    char* dimensions;
+    int eight;
+    int ten;
+    int twelve;
+    float price_eight;
+    float price_ten;
+    float price_twelve;
+}lumber;
